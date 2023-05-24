dce8b8c727012410d647abd712a426a2423df10f
GhenGhenBoy <daveghenghen@gmail.com>
2023-05-24 07:57:27 +0100

    hass router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "swiper/swiper.min.css";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./App.scss";
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -16,7 +16,7 @@ function App() {
 
   
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route
         render={(props) => (
           <>
@@ -29,7 +29,7 @@ function App() {
           </>
         )}
       />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
